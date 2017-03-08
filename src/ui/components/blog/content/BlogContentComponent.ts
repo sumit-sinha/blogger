b6f74128fed7a4217bf7dc79ee460b87e9330086
Sumit Sinha <sumit.sinha.email@gmail.com>
2017-03-08 00:20:18 +0000

    fixing issue with user name hyperlink

--- src/ui/components/blog/content/BlogContentComponent.ts
+++ src/ui/components/blog/content/BlogContentComponent.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
 	selector: "blog-content",
 	template: `
 		<h1>{{ content.title }}</h1>
 		<p class="lead">
-			by <a href="{{ content.author.link }}">{{ content.author.name }}</a>
+			by <a href="javascript:void(0);" (click)="onLinkClick($event, content.author.link)">{{ content.author.name }}</a>
 		</p>
 		<hr>
 		<div [innerHTML]="content.text"></div>
@@ -16,4 +17,15 @@ export class BlogContentComponent {
 
 	@Input()
 	content: Object;
+
+	constructor(private router: Router) { }
+
+	/**
+	 * function called when hyperlink is clicked
+	 * @param $event {Object}
+	 * @param url {String} url to navigate to
+	 */
+	onLinkClick($event, url) {
+		this.router.navigateByUrl(url);
+	}
 }
